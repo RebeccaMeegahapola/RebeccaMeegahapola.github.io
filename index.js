19dccd98f6c16be34b0a6fa995295dfa8e43858c
RebeccaMeegahapola <rebeccameegahapola@gmail.com>
2024-06-19 09:57:11 +0530

    up

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//Home banner Animation
+
 particlesJS("particles-js", {
   "particles": {
     "number": {
@@ -175,3 +177,27 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+/*projects */
+$(".filter a").click(function (e) {
+  e.preventDefault();
+  var a = $(this).attr("href");
+  a = a.substr(1);
+  $(".sets a").each(function () {
+    if (!$(this).hasClass(a) && a != "") $(this).addClass("hide");
+    else $(this).removeClass("hide");
+  });
+
+
+
+// Add active class to the current button (highlight it)
+var btnContainer = document.getElementById("btncontainer");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  var current = document.getElementsByClassName("btn-active");
+  current[0].className = current[0].className.replace(" btn-active", "");
+  this.className += " btn-active";
+
+}
+});
+
